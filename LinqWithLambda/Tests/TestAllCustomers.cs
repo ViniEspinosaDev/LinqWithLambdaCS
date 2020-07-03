720841d17438485645596381f28a5a6349716036
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2020-07-03 10:57:59 -0300

    Criando interface teste e fazendo testes

---
+++ LinqWithLambda/Tests/TestAllCustomers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqWithLambda.Tests
+{
+    public class TestAllCustomers : ITest
+    {
+        public void Test()
+        {
+            var customers = DataBase.DataBase.GetCustomers();
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+            }
+        }
+    }
+}
