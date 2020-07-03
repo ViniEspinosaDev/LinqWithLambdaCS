7afabdce430979d87c6fd2d0bf2e02f692ca1901
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2020-07-03 10:46:03 -0300

    Vinculando projeto ao Git

---
+++ LinqWithLambda/DataBase/DataBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqWithLambda.DataBase
+{
+    public class DataBase
+    {
+        public int MyProperty { get; set; }
+
+    }
+}
---
+++ LinqWithLambda/Model/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LinqWithLambda.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Decimal TotalValue { get; set; }
+    }
+}
